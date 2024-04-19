ab3312843e06b8408760e11215fcaf7eedd9a315
YashCodeShit <152263833+YashCodeShit@users.noreply.github.com>
2024-04-20 05:24:02 +0530

    added index.html file

---
+++ script.js
@@ -0,0 +1,24 @@
+const text = document.getElementById('text');
+const leaf = document.getElementById('leaf');
+const hill1 = document.getElementById('hill1');
+const hill4 = document.getElementById('hill4');
+const hill5 = document.getElementById('hill5');
+
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('scroll', () => {
+    const value = window.scrollY;
+    text.style.marginTop = `${value * 1.5}px`;
+    leaf.style.Top = `${value * -1.5}px`;
+    leaf.style.left = `${value * 1.5}px`;
+
+    hill5.style.left = `${value * 1.5}px`;
+
+    hill4.style.left = `${value * -1.5}px`;
+
+    hill1.style.top = `${value * 1.5}px`;
+
+    
+
+
+});
+});
